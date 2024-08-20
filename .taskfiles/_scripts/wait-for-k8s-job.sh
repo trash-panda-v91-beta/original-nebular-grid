b11d2d7ddd40ee677857db2665c5bfa3e0da7fe2
aka-raccoon <42897550+aka-raccoon@users.noreply.github.com>
2024-08-20 22:55:46 +0200

    fix(task): update rook tasks

--- .taskfiles/_scripts/wait-for-k8s-job.sh
+++ .taskfiles/_scripts/wait-for-k8s-job.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-JOB_NAME=$1
-NAMESPACE="${2:-default}"
-CLUSTER=$3
+JOB_NAME=${1}
+NAMESPACE=${2}
+CLUSTER=${3}
 
 [[ -z "${JOB_NAME}" ]] && echo "Job name not specified" && exit 1
 
 while true; do
-  STATUS="$(kubectl --context "${CLUSTER}" -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
-  printf "Job status: %s\n" "${STATUS}"
-  if [[ "${STATUS}" == "Pending" ]]; then
+  STATUS="$(kubectl --context "${CLUSTER}" -n "${NAMESPACE}" get pods -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
+  if [[ "${STATUS}" == "Pending" || "${STATUS}" == "Succeeded" ]]; then
     break
   fi
   sleep 1
